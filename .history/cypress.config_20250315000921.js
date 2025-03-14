d090a60cf69594bc1f1e44a34a082004111a362d
Renata Smuczak <renata.smuczak@gmail.com>
2025-03-15 00:13:42 +0100

    clean up

--- .history/cypress.config_20250315000921.js
+++ .history/cypress.config_20250315000921.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "3bzcct",
+  projectId: '3bzcct',
   viewportHeight: 800,
   viewportWidth: 1200,
+
   e2e: {
-    baseUrl: "https://tapsshop.pl/",
-  },
+    baseUrl:  'https://tapsshop.pl/'
+  }
 });
